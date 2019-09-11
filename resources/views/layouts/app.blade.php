49869c322da4cbd2828480d0533da8c36fda3dba
Juniper McIntyre <junipermcintyre@gmail.com>
2019-09-11 13:30:23 -0400

    Put logout at bottom of nav dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,11 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('cashcollections.create') }}">New collection</a>
+                                    <a class="dropdown-item" href="{{ route('cashcollections.index') }}">Old collections</a>
+                                    <a class="dropdown-item" href="#">Export</a>
+                                    <a class="dropdown-item" href="#">Account</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -64,11 +69,6 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-
-                                    <a class="dropdown-item" href="{{ route('cashcollections.create') }}">New collection</a>
-                                    <a class="dropdown-item" href="{{ route('cashcollections.index') }}">Old collections</a>
-                                    <a class="dropdown-item" href="#">Export</a>
-                                    <a class="dropdown-item" href="#">Account</a>
                                 </div>
                             </li>
                         @endguest
